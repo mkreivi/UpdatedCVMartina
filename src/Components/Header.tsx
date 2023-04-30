bd601dff0df09cba0190471a204d878868a45b8c
Martina Kreivi <martinakreivi@gmail.com>
2023-04-30 20:41:35 +0200

    flex box 1 and 3

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -10,19 +10,26 @@ import {
 
 function Header() {
   return (
-    <Box bg="#494E5F" h="170px" color="#FFE7D4">
+    // HELA HEADERN
+    <Box h="170px" bg="#494E5F" color="#FFE7D4">
       <Flex justify={"space-between"} align={"center"}>
-        <Box p="20px" h="170px">
-          Meny
-          <HamburgerIcon boxSize={10} color="#FFE7D4" />
-        </Box>
-
-        <Box>
-          <Image src="src/assets/LogoMartina.svg" />
+        <Box p="20px" w="160px">
+          <FormControl display="flex" alignItems="center">
+            <FormLabel htmlFor="Menu" mb="0">
+              Meny
+              <HamburgerIcon ml="7px" boxSize={6} color="#FFE7D4" />
+            </FormLabel>
+          </FormControl>
         </Box>
 
         <Flex align={"center"}>
-          <Box p="20px">
+          <Box mx="auto">
+            <Image src="src/assets/LogoMartina.svg" />
+          </Box>
+        </Flex>
+
+        <Flex align={"center"}>
+          <Box p="20px" w="160px">
             <FormControl display="flex" alignItems="center">
               <FormLabel htmlFor="Swe-Eng language" mb="0">
                 Sve - Eng
