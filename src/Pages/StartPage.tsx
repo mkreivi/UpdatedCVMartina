e3dce2cc7ba0e85a7c4baf48eda76b5a0b802dfa
Martina Kreivi <martinakreivi@gmail.com>
2023-04-30 21:49:20 +0200

    startpage flex box1 and image (wip)

--- src/Pages/StartPage.tsx
+++ src/Pages/StartPage.tsx
@@ -1,4 +1,21 @@
+import { Box, Image } from "@chakra-ui/react";
+
 function StartPage() {
-  return <main></main>;
+  return (
+    <Box m="20px" h="100%" display="flex" alignItems="flex-start">
+      <Box ml="50px" bg="#CCCCCC">
+        Massa text Massa text Massa text Massa text Massa text Massa text Massa
+        text Massa text Massa text
+      </Box>
+      <Box mr="50px">
+        <Image
+          src="src/assets/MartinaBlackWhite.JPG"
+          borderRadius="full"
+          boxSize="300px"
+          objectFit="cover"
+        />
+      </Box>
+    </Box>
+  );
 }
 export default StartPage;
