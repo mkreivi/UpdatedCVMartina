fcb100072ac6b6f5b61dc1f511a4048927c475db
Martina Kreivi <martinakreivi@gmail.com>
2023-04-30 17:35:07 +0200

    Setup outlet mm

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>Hello World</h1>
+      <Outlet />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -22,6 +22,7 @@ const router = createBrowserRouter(
     </Route>
   )
 );
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
