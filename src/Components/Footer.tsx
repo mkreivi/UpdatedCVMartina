6e5058b0971409a73c81e02cb26e46f52d8fc72f
Martina Kreivi <martinakreivi@gmail.com>
2023-05-01 16:05:31 +0200

    Footer links

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,7 +1,41 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Image, Link } from "@chakra-ui/react";
 
 function Footer() {
-  return <Box bg="#494E5F" h="170px" color="#FFE7D4"></Box>;
+  return (
+    <Box bg="##494E5F" h="170px" color="#FFE7D4">
+      <Flex justify={"center"}>
+        <Link href="mailto:martina.kreivi@medieinstitutet.se">
+          <Image
+            src="src/assets/iconMail150px.svg"
+            borderRadius="full"
+            boxSize="80px"
+            objectFit="cover"
+            p="7px"
+            m="5px"
+          />
+        </Link>
+        <Link href="https://github.com/mkreivi">
+          <Image
+            src="src/assets/IconGithub.png"
+            borderRadius="full"
+            boxSize="80px"
+            objectFit="cover"
+            p="7px"
+            m="5px"
+          />
+        </Link>
+        <Link href="https://www.linkedin.com/in/martina-kreivi-263032b9/">
+          <Image
+            src="src/assets/iconLinkdIn.png"
+            boxSize="80px"
+            objectFit="cover"
+            p="7px"
+            m="5px"
+          />
+        </Link>
+      </Flex>
+    </Box>
+  );
 }
 
 export default Footer;
