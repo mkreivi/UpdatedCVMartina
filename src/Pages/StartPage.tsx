3356449e30defef80ac197ee46fba60e311397f2
Martina Kreivi <martinakreivi@gmail.com>
2023-05-01 10:06:51 +0200

    Position right but need justification

--- src/Pages/StartPage.tsx
+++ src/Pages/StartPage.tsx
@@ -1,21 +1,42 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 
 function StartPage() {
   return (
-    <Box m="20px" h="100%" display="flex" alignItems="flex-start">
-      <Box ml="50px" bg="#CCCCCC">
-        Massa text Massa text Massa text Massa text Massa text Massa text Massa
-        text Massa text Massa text
+    <main>
+      <Box>
+        <Text
+          bg="#FFE7D4"
+          justifyContent="space-between"
+          h={["100%"]}
+          fontSize={["12px", "16px", "20px"]}
+          m="30px"
+        >
+          Välkommen
+          <Flex>
+            <Text
+              p="10px"
+              m="30px"
+              bg="#CCCCCC"
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              Massa text Massa text Massa text Massa text Massa text Massa text
+              Massa text Massa text Massa text
+            </Text>
+            <Box mr="50px">
+              <Image
+                src="src/assets/MartinaBlackWhite.JPG"
+                borderRadius="full"
+                boxSize="200px"
+                objectFit="cover"
+                m="10px"
+              />
+            </Box>
+          </Flex>
+        </Text>
       </Box>
-      <Box mr="50px">
-        <Image
-          src="src/assets/MartinaBlackWhite.JPG"
-          borderRadius="full"
-          boxSize="300px"
-          objectFit="cover"
-        />
-      </Box>
-    </Box>
+    </main>
   );
 }
 export default StartPage;
