3de26bfa3b251f26b772553cb84cdc8e1d7655ee
Martina Kreivi <martinakreivi@gmail.com>
2023-05-01 11:53:52 +0200

    setup portfolio.tsx

--- src/Pages/Portfolio.tsx
+++ src/Pages/Portfolio.tsx
@@ -1,7 +1,48 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+
 function Portfolio() {
   return (
     <main>
-      <div>Portfolio</div>
+      <Box className="Hallo">
+        <Text
+          bg="#F5F5F5"
+          justifyContent="space-between"
+          alignContent="center"
+          h={["100%"]}
+          fontSize={["12px", "16px", "20px"]}
+          m="30px"
+          p="10px"
+        >
+          <Text
+            className="HeadingWelcome"
+            bg="#FFE7D4"
+            h="50px"
+            display="flex"
+            alignItems="center"
+            p="20px"
+          >
+            Portfolio
+          </Text>
+          <Flex
+            className="TextAndPictureTogether"
+            alignContent="flex-end"
+            justifyContent="space-between"
+            align="center"
+          >
+            <Text
+              className="Introduction"
+              p="10px"
+              m="30px"
+              bg="#F5F5F5"
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              Här har jag samlat projekt jag gjort
+            </Text>
+          </Flex>
+        </Text>
+      </Box>
     </main>
   );
 }
