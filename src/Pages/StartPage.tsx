47c9bee036b309ff1e8d4f2e626e2803b7cf8b2c
Martina Kreivi <martinakreivi@gmail.com>
2023-05-01 10:38:42 +0200

    getting there!

--- src/Pages/StartPage.tsx
+++ src/Pages/StartPage.tsx
@@ -3,18 +3,34 @@ import { Box, Flex, Image, Text } from "@chakra-ui/react";
 function StartPage() {
   return (
     <main>
-      <Box>
+      <Box className="Hallo">
         <Text
           bg="#F5F5F5"
           justifyContent="space-between"
+          alignContent="center"
           h={["100%"]}
           fontSize={["12px", "16px", "20px"]}
           m="30px"
           p="10px"
         >
-          <Text bg="#FFE7D4">Välkommen</Text>
-          <Flex>
+          <Text
+            className="HeadingWelcome"
+            bg="#FFE7D4"
+            h="50px"
+            display="flex"
+            alignItems="center"
+            p="20px"
+          >
+            Välkommen
+          </Text>
+          <Flex
+            className="TextAndPictureTogether"
+            alignContent="flex-end"
+            justifyContent="space-between"
+            align="center"
+          >
             <Text
+              className="Introduction"
               p="10px"
               m="30px"
               bg="#F5F5F5"
@@ -26,15 +42,16 @@ function StartPage() {
               Studerar FrontEnd Development på Medieinstitutet i Göteborg
               <br />
               <br />
-              "Jag är en positiv, nyfiken och strukturerad medarbetare som
-              älskar att anpassa och modernisera webbplatser för att form och
-              funktion ska tilltala användaren!"
+              "Jag är en positiv, nyfiken och strukturerad medarbetare <br />
+              som älskar att anpassa och modernisera webbplatser <br />
+              för att form och funktion ska tilltala användaren!"
             </Text>
-            <Box mr="50px">
+            <Box mr="50px" className="ThisOne">
               <Image
+                className="Picture"
                 src="src/assets/MartinaBlackWhite.JPG"
                 borderRadius="full"
-                boxSize="150px"
+                boxSize="180px"
                 objectFit="cover"
                 m="10px"
               />
