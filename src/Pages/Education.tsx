0d2dbe3e26338fe858a30bfa8c977ff66445fa24
Martina Kreivi <martinakreivi@gmail.com>
2023-05-01 19:18:06 +0200

    Education added

--- src/Pages/Education.tsx
+++ src/Pages/Education.tsx
@@ -1,6 +1,90 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Divider, Flex, Text } from "@chakra-ui/react";
 
 function Education() {
-  return <Box></Box>;
+  return (
+    <main>
+      <Box p="30px">
+        <Text bg="#F5F5F5" fontSize={["12px", "16px", "20px"]} p="10px">
+          <Text
+            fontSize="24px" // Byt storlek på denna!
+            bg="#FFE7D4"
+            h="50px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            p="20px"
+            mx="auto"
+          >
+            Utbildning
+          </Text>
+          <Flex
+            flexDirection="column"
+            className="TextAndPictureTogether"
+            align="center"
+          >
+            <Text
+              fontSize="24px"
+              className="HeadingEducation"
+              p="10px"
+              m="30px"
+              mb="0"
+              bg="#F5F5F5"
+              textAlign="center"
+            >
+              Studerar FrontEnd Development på Medieinstitutet i Göteborg
+              <Text fontSize="20px">Aug 2022 - Maj 2024</Text>
+            </Text>
+            <Text
+              className="CoursesEducation"
+              p="10px"
+              m="0px, 30px"
+              textAlign="center"
+            >
+              Kurser jag läst när jag påbörjar min LIA i september 2024 <br />
+              <br />
+              HTML/CSS • UX/Usability • JavaScript Grund • JavaScript
+              Fördjupning • Dynamisk Webbutveckling • Grafiska verktyg för
+              gränssnittsdesign • Projekt med agila metoder <br />
+              <br />
+            </Text>
+            <Divider
+              orientation="horizontal"
+              borderColor="brand.400"
+              w={["100%"]}
+            />
+            <Text
+              fontSize="24px"
+              className="HeadingEducation"
+              p="10px"
+              m="30px"
+              mb="0"
+              textAlign="center"
+            >
+              IT/Media programmet på Hulebäcksgymnasiet i Mölnlycke
+            </Text>
+            <Text fontSize="20px">Aug 2022 - Maj 2024</Text>
+            <Text
+              className="CoursesEducation"
+              p="10px"
+              m="0px, 30px"
+              textAlign="center"
+            >
+              Urval av kurser jag läste: <br />
+              <br />
+              Grafisk kommunikation • Webbdesign • Mediekunskap •
+              Mediekommunikation • Medieproduktion • Medieteknik • Multimedia{" "}
+              <br />
+              <br />
+            </Text>
+            <Divider
+              orientation="horizontal"
+              borderColor="brand.400"
+              w={["100%"]}
+            />
+          </Flex>
+        </Text>
+      </Box>
+    </main>
+  );
 }
 export default Education;
