4b4eec7abc4fd11c6f11f036e86495a951129bd0
Martina Kreivi <martinakreivi@gmail.com>
2023-05-01 12:28:55 +0200

    Added two cards in portfolio

--- src/Pages/Portfolio.tsx
+++ src/Pages/Portfolio.tsx
@@ -1,4 +1,15 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Heading,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
 function Portfolio() {
   return (
@@ -23,27 +34,100 @@ function Portfolio() {
           >
             Portfolio
           </Text>
-          <Flex
-            className="TextAndPictureTogether"
-            alignContent="flex-end"
+
+          <Text
+            className="Introduction"
+            p="10px"
+            m="30px"
+            bg="#F5F5F5"
+            display="flex"
+            alignItems="center"
             justifyContent="space-between"
-            align="center"
           >
-            <Text
-              className="Introduction"
-              p="10px"
-              m="30px"
-              bg="#F5F5F5"
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-            >
-              Här har jag samlat projekt jag gjort
-            </Text>
-          </Flex>
+            Här har jag samlat projekt jag gjort
+          </Text>
+          <SimpleGrid
+            columns={{ base: 1, sm: 2 }}
+            gap={{ base: "20px", sm: "40px" }}
+            alignItems="center"
+            mb={{ base: "20px", sm: "0" }}
+          >
+            <Box>
+              <Card m="10px" variant="outline">
+                <Image
+                  p="10px"
+                  src="src/assets/LisebergUXUI.png"
+                  objectFit="cover"
+                />
+                <Stack>
+                  <CardBody>
+                    <Button
+                      borderRadius="20px"
+                      variant="solid"
+                      bg="#B398ED"
+                      color="#F5F5F5"
+                    >
+                      UX/UI
+                    </Button>
+                    <Heading mt="10px" size="md">
+                      Gränssnittsanalys - Förbättra UX/UI
+                    </Heading>
+
+                    <Text py="2">
+                      Skolprojekt där jag förbättrat UX på Lisebergs hemsida i
+                      Figma.
+                    </Text>
+                    <Text py="1">Oktober 2022</Text>
+                  </CardBody>
+
+                  <CardFooter>
+                    <Button variant="solid" bg="#494E5F" color="#F5F5F5">
+                      Se mer
+                    </Button>
+                  </CardFooter>
+                </Stack>
+              </Card>
+
+              <Card m="10px" variant="outline">
+                <Image
+                  p="10px"
+                  src="src/assets/KorpensHantverksbutik.png"
+                  objectFit="cover"
+                />
+                <Stack>
+                  <CardBody>
+                    <Button
+                      borderRadius="20px"
+                      variant="solid"
+                      bg="#7BABF2"
+                      color="#F5F5F5"
+                    >
+                      HTML/CSS
+                    </Button>
+                    <Heading mt="10px" size="md">
+                      Modernisera en hemsida - Grupparbete
+                    </Heading>
+
+                    <Text py="2">
+                      Skolprojekt där jag tillsammans med två klasskamrater
+                      förbättrat gränssnittet på en befintlig hemsida.
+                    </Text>
+                    <Text py="1">Oktober 2022</Text>
+                  </CardBody>
+
+                  <CardFooter>
+                    <Button variant="solid" bg="#494E5F" color="#F5F5F5">
+                      Se mer
+                    </Button>
+                  </CardFooter>
+                </Stack>
+              </Card>
+            </Box>
+          </SimpleGrid>
         </Text>
       </Box>
     </main>
   );
 }
+
 export default Portfolio;
