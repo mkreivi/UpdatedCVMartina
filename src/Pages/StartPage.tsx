f04414ad7ac0153a4540b87717db4de22e7ae2b9
Martina Kreivi <martinakreivi@gmail.com>
2023-05-01 14:10:33 +0200

    Text startpage (wip)

--- src/Pages/StartPage.tsx
+++ src/Pages/StartPage.tsx
@@ -3,7 +3,7 @@ import { Box, Flex, Image, Text } from "@chakra-ui/react";
 function StartPage() {
   return (
     <main>
-      <Box className="Hallo">
+      <Box className="StartPageMain">
         <Text
           bg="#F5F5F5"
           justifyContent="space-between"
@@ -56,6 +56,60 @@ function StartPage() {
                 m="10px"
               />
             </Box>
+          </Flex>
+        </Text>
+      </Box>
+      <Box className="StartPageMain">
+        <Text
+          bg="#F5F5F5"
+          justifyContent="space-between"
+          alignContent="center"
+          h={["100%"]}
+          fontSize={["12px", "16px", "20px"]}
+          m="30px"
+          p="10px"
+        >
+          <Text
+            className="HeadingWelcome"
+            bg="#FFE7D4"
+            h="50px"
+            display="flex"
+            alignItems="center"
+            p="20px"
+          ></Text>
+          <Flex
+            className="TextAndPictureTogether"
+            alignContent="flex-end"
+            justifyContent="space-between"
+            align="center"
+          >
+            <Box mr="50px" className="ThisOne">
+              <Image
+                className="Picture"
+                src="src/assets/MartinaK.PNG"
+                borderRadius="full"
+                boxSize="180px"
+                objectFit="cover"
+                m="10px"
+              />
+            </Box>
+            <Text
+              className="Introduction"
+              p="10px"
+              m="30px"
+              bg="#F5F5F5"
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              fontStyle="italic"
+            >
+              Varför FrontEnd Development?
+              <br />
+              <br />
+              Under gymnasiet 2005-2008 läste jag IT/Media programmet och
+              fastnade för webbdesign och webbutveckling. Sedan dess har jag
+              drömmt om att jobba med just detta men
+            </Text>
           </Flex>
         </Text>
       </Box>
