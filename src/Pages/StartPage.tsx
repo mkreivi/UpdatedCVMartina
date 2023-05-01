b3f858d9780be111bc21a2d62efbf364947c6a8e
Martina Kreivi <martinakreivi@gmail.com>
2023-05-01 10:21:14 +0200

    css styling

--- src/Pages/StartPage.tsx
+++ src/Pages/StartPage.tsx
@@ -5,30 +5,36 @@ function StartPage() {
     <main>
       <Box>
         <Text
-          bg="#FFE7D4"
+          bg="#F5F5F5"
           justifyContent="space-between"
           h={["100%"]}
           fontSize={["12px", "16px", "20px"]}
           m="30px"
+          p="10px"
         >
-          Välkommen
+          <Text bg="#FFE7D4">Välkommen</Text>
           <Flex>
             <Text
               p="10px"
               m="30px"
-              bg="#CCCCCC"
+              bg="#F5F5F5"
               display="flex"
               alignItems="center"
               justifyContent="space-between"
+              fontStyle="italic"
             >
-              Massa text Massa text Massa text Massa text Massa text Massa text
-              Massa text Massa text Massa text
+              Studerar FrontEnd Development på Medieinstitutet i Göteborg
+              <br />
+              <br />
+              "Jag är en positiv, nyfiken och strukturerad medarbetare som
+              älskar att anpassa och modernisera webbplatser för att form och
+              funktion ska tilltala användaren!"
             </Text>
             <Box mr="50px">
               <Image
                 src="src/assets/MartinaBlackWhite.JPG"
                 borderRadius="full"
-                boxSize="200px"
+                boxSize="150px"
                 objectFit="cover"
                 m="10px"
               />
